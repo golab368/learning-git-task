6eaa6b5e1df0cfd133d2d49fc95ba4a31b23d244
Milosz Golebiowski <golebiowski.mg@gmail.com>
2020-10-22 20:03:25 +0000

    dodalem plik lista zakupow

---
+++ lista_zakupow.py
@@ -0,0 +1,26 @@
+store = {"biedronka":"chleb"}
+
+for key,values in store.items():
+    print("ide do " +key.capitalize()+ " i kupuje " + values.capitalize())
+print("\n")
+"""problem jest gdy w slowniku do klucza podajemy wiele wartosci
+jak zrobic piersza dużą litere dla krotki lub listy w słowniku??
+"""
+
+sotre1 = {"biedronka":["chleb","bulki","przyprawy"], 'lidl': ['skarpety', 'playstation5']}
+
+sum_of_products = 0git
+for store_name, products in sotre1.items():
+    im_going_to = store_name.capitalize()
+    # capitalized_products = []
+    # for product in products:
+    #     capitalized_products.append(product.capitalize())
+    capitalized_products = [asd.capitalize() for asd in products]
+    i_want_to_buy = ', '.join(capitalized_products)
+    
+    #sum_of_products = sum_of_products + len(products)
+    sum_of_products += len(products)
+
+    print("Idę do "+im_going_to+ " Kupić "+i_want_to_buy )  
+
+print(f"Moja lista zakupów składa się z {sum_of_products} rzeczy" )
